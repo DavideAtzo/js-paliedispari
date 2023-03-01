550010593930f3fc513fad673a09db0aef8117e1
DavideAtzo <atzo9477@gmail.com>
2023-03-01 18:14:57 +0100

    scelta numero giocatore

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -2,10 +2,21 @@
 
 // variabile che conterrà la risposta dell'utente 
 let risposta = '';
-// chiedi con prompt la sua scelta 
+let rispostaNum = '';
+// chiedi con prompt la sua scelta pari o dispari
 do {
     risposta = prompt('Scrivi la tua scelta tra pari o dispari ');
 }
 // WHILE se la risposta non contiene pari e dispari continua a chiedere
 while (risposta != 'pari' && risposta != 'dispari');
 console.log(risposta);
+
+
+let numGiocatore = '';
+// chiedi con prompt un numero da 1 a 5
+do {
+    numGiocatore = prompt('Scegli un numero da 1 a 5 ');
+}
+// WHILE se il numero scelto non è compreso tra quelli sotto citati, continua  a chiedere
+while (numGiocatore <= 1 || numGiocatore > 5);
+console.log(numGiocatore);
